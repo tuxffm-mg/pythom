f05a97a9b4ce9409b142a23978e44f6e7f9b19b1
tuxffm-mg <tuxffm@gmail.com>
2024-09-30 08:28:04 +0200

    Create file.sort.py

---
+++ file.sort.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+os.system('cd c:\Users\GoeMa05-ext\Downloads')
+images = [f for f in os.listdir() if '.jpg' in f.lower()]
+
+os.mkdir('downloaded_images')
+
+for image in images:
+    new_path = 'downloaded_images/' + image
+    shutil.move(image, new_path)
